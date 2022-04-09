32db84534abc3243425ad237edccda40ca1321e0
Martin Duvergey <martin.duvergey@gmail.com>
2022-04-09 22:10:13 +0200

    Object pool editor (WIP)

---
+++ Editor/ObjectPoolEditor.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Marty
+{
+    [CustomEditor(typeof(ObjectPool))]
+    public class ObjectPoolEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
